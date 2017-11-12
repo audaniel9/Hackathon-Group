15566a731c45f99def0bb8af69e2180d54591eea
russ226 <russ2510@gmail.com>
2017-11-12 12:17:56 -0500

    fix spelling errors andfixed relationship between jobs and shelters

--- people/models/models.py
+++ people/models/models.py
@@ -31,6 +31,7 @@ from sqlalchemy.orm import validates
 db = SQLAlchemy()
 
 class Users(db.Model):
+    __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=True, nullable=False)
@@ -59,7 +60,8 @@ class Users(db.Model):
         return zip
 
 class Shelters(db.Model):
-    ahelter_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    __tablename__ = 'shelters'
+    shelter_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=True, nullable=False)
     shelter_name = db.Column(db.String(200), unique=True, nullable=False)
@@ -68,6 +70,7 @@ class Shelters(db.Model):
     city = db.Column(db.String(50), nullable=False)
     zip_code = db.Column(db.Integer, nullable=False)
     address = db.Column(db.String(500), nullable=False)
+
 
     def validate_password(self, password):
         return bcrypt.verify(password, self.password)
@@ -84,3 +87,20 @@ class Shelters(db.Model):
     def check_zip_code(self,key,zip):
         assert len(str(zip)) == 5
         return zip
+
+class Jobs(db.Model):
+        __tablename__ = 'jobs'
+        #location shelter name and all that other stuff should be comming
+        id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+        job_disc = db.Column(db.String(500), unique=True, nullable=False)
+        shelter = db.relationship('Shelters',backref=db.backref("shelter", uselist=False))
+        shelter_id = db.Column(db.Integer, db.ForeignKey('shelters.shelter_id'),unique=True,nullable=False)
+        position = db.Column(db.String(500), nullable=False)
+        position_req = db.Column(db.String(500), unique = True, nullable=False)
+
+
+        def __repr__(self):
+            return "<Users( postion='%s', shelter_id='%s', email_address='%s')>" % (self.position, self.shelter_id)
+
+        def shelter_vef(self):
+            return Shelters.query.filter(shelter_id=self.shelter_id) == 1

4f824e8d4e8522b78d1be1197cf80d86fbce0f5a
swoo328 <31424334+swoo328@users.noreply.github.com>
2017-11-11 20:11:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 from flask import Flask
-from flask import Flask, request, session, flash, render_template
+from flask import Flask, request, session, flash, render_template, redirect, url_for
 
 @app.route('/')
 def homepage():
-    return render_template('URL.html')
+    return render_template('index.html')
     
 @app.route('/login/', methods = ["POST", "GET"])
 def login():
     if request.methods == "POST":
         POST_USERNAME = str(request.form['username'])
         POST_PASSWORD = str(request.form['password'])
-
-    if POST_USERNAME == 'username' and POST_PASSWORD  == 'password': 
-            #session['logged_in'] = true:
-        return redirect(url_for( 'url.html'))               
-    else:
+   if registered_user = User.query.filter_by(username=POST_USERNAME,password=POST_PASSWORD).first()
+   return redirect(for_url( 'url.html'))
+   else:
         flash('Invalid credientials!. Try Again.')
 
     #else if request == 'GET'                            
@@ -22,7 +20,7 @@ def login():
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
-    return homepage()
+    return render_template('index.html')
     
 
 
